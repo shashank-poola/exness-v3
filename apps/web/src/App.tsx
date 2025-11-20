466f133c5434dd5436a042e91b5012ec11248919
shashank-poola <shashankpoola123@gmail.com>
2025-11-20 14:31:26 +0530

    fix in packages/db

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,10 +1,10 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import TradingPage from "./pages/TradingPage";
-import DocsPage from "./pages/DocsPage";
-import AuthPage from "./pages/AuthPage";
-import NotFound from "./pages/NotFound";
+import LandingPage from "./pages/LandingPage.js";
+import TradingPage from "./pages/TradingPage.js";
+import DocsPage from "./pages/DocsPage.js";
+import AuthPage from "./pages/AuthPage.js";
+import NotFound from "./pages/NotFound.js";
 
 const queryClient = new QueryClient();
 
