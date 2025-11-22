54bb67bf06076f82c10d05d6b9518f2e69bcff30
shashank-poola <shashankpoola123@gmail.com>
2025-11-22 14:52:26 +0530

    frontend url mismatched

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,90 +1,26 @@
 import express from 'express';
-import nodemailer from "nodemailer";
-import jwt from 'jsonwebtoken';
-import cookieParser from "cookie-parser";
-import dotenv from 'dotenv';
+import cors from 'cors';
+import mainRouter from './routes';
+import cookieParser from 'cookie-parser';
 
-dotenv.config();
+const PORT = process.env.PORT || 4000;
 
-const app = express()
-const JWT_TOKEN = "secret"
+const app = express();
 
-app.use(express.json())
-;
+const FE_URL = process.env.CORS_ORIGIN || 'http://localhost:5173';
 
-app.use(cookieParser())
-app.post("/api/v1/signup", async (req, res) => {
-    const { email } = req.body
+app.use(express.json());
+app.use(
+  cors({
+    origin: FE_URL,
+    credentials: true,
+  })
+);
 
-    if (!email) {
-        res.status(404).json({
-            message: "email not found"
-        })
-        return
-    }
+app.use(cookieParser());
 
-    const token = jwt.sign(email, JWT_TOKEN)
-    const link = `http://localhost:8000/token=${token}`
-    try {
+app.use('/api/v1', mainRouter);
 
-        const transporter = nodemailer.createTransport({
-            host: "smtp.gmail.com",
-            port: 587,
-            secure: false,
-            auth: {
-                user: "shashankpoola123@gmail.com",
-                pass: "blga tlbb jkkq aung",
-            },
-        });
-
-
-        const info = await transporter.sendMail({
-            from: '"S30" <shashankpoola123@gmail.com>',
-            to: email,
-            subject: "super30-contest ✔",
-            text: "Hello world?",
-            html: `<b>Please sign in?</b> ${link}`,
-        });
-
-        console.log("Message sent:", info.messageId);
-
-        return res.status(200).json({
-            message: "Signup email sent successfully",
-            link,
-        })
-
-    } catch (error) {
-        console.log(error)
-    }
-
-})
-
-app.get("/token=:token", async (req, res) => {
-    const { token } = req.params
-
-    if (!token) {
-        res.status(403).json({
-            message: "token not found"
-        })
-        return
-    }
-
-    const decode = jwt.verify(token, JWT_TOKEN)
-    if (!decode) {
-        res.status(405).json({
-            message: "login unsucessfull"
-        })
-    }
-    
-    res.cookie("auth",token,{
-        httpOnly:true
-    })
-    res.status(200).json({
-        message:"login successfull",
-        user:decode
-    })
-})
-
-app.listen(8000, () => {
-    console.log(`server is running on 8000`)
-})
+app.listen(PORT, () => {
+  console.log('Server started');
+});
