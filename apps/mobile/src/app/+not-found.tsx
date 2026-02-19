656cf0c0f7504c05ac2ef99c2db0cf4dd4a5c4a1
shashank poola <shashankpoola123@gmail.com>
2026-02-19 22:18:49 +0530

    feat(mobile): created styles folder

--- apps/mobile/src/app/+not-found.tsx
+++ apps/mobile/src/app/+not-found.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { usePathname, useRouter } from "expo-router";
+import { styles } from "../styles/notfound.style";
 
 export default function NotFound() {
   const pathname = usePathname();
@@ -20,28 +21,3 @@ export default function NotFound() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#f3f4f6",
-    gap: 8,
-  },
-  code: {
-    fontSize: 64,
-    fontWeight: "bold",
-    color: "#111",
-  },
-  title: {
-    fontSize: 18,
-    color: "#6b7280",
-  },
-  link: {
-    marginTop: 8,
-    fontSize: 15,
-    color: "#3b82f6",
-    textDecorationLine: "underline",
-  },
-});
---
+++ apps/mobile/src/styles/notfound.style.ts
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#f3f4f6",
+    gap: 8,
+  },
+  code: {
+    fontSize: 64,
+    fontWeight: "bold",
+    color: "#111",
+  },
+  title: {
+    fontSize: 18,
+    color: "#6b7280",
+  },
+  link: {
+    marginTop: 8,
+    fontSize: 15,
+    color: "#3b82f6",
+    textDecorationLine: "underline",
+  },
+});
