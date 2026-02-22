a5582a23bd96d8e31bd88b2b2431b54bae5dbb46
shashank poola <shashankpoola123@gmail.com>
2026-02-22 15:24:55 +0530

    refactor(api): authmiddleware refactor

--- apps/api/src/middleware/auth.middleware.ts
+++ apps/api/src/middleware/auth.middleware.ts
@@ -4,42 +4,49 @@ import jwt from "jsonwebtoken";
 declare global {
   namespace Express {
     interface Request {
-      user?: string; // or { email: string } if you want an object later
+      user?: string;
     }
   }
 }
 
-export async function authMiddleware(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
+export async function authMiddleware( req: Request, res: Response, next: NextFunction) {
   try {
     
-    // Expect header: Authorization: Bearer <token>
     const authHeader = req.headers.authorization;
 
     if (!authHeader) {
-      return res.status(401).json({ error: "Authorization header missing" });
+      res.status(401).json({
+        success: false,
+        message: null,
+        error: "AUTHORIZED_HEADER_MISSING" 
+      })
+      return;
     }
 
     const [type, token] = authHeader.split(" ");
 
     if (type !== "Bearer" || !token) {
-      return res.status(401).json({ error: "Invalid authorization format" });
+      return res.status(401).json({ 
+        success: false,
+        message: null,
+        error: "INVALID_TOKEN, BEARER_TOKEN_REQUIRED"
+      });
     }
 
-    // Verify JWT
     const payload = jwt.verify(token, process.env.JWT_SECRET!) as {
       email: string;
     };
 
-    // Attach to request
     req.user = payload.email;
 
     next();
   } catch (err) {
     console.error('JWT Verification Error:', err);
-    return res.status(401).json({ error: "Invalid or expired token" });
+    res.status(500).json({
+      success: false,
+      message: null,
+      error: "INTERNAL_SERVER_ERROR" 
+    })
+    return;
   }
 }
